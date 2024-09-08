d5b93c5ffac9df17e2d6bc02b4ea808057e19240
Dmitrii Pilyuk <dmitriipiliuk@gmail.com>
2024-09-08 19:08:52 +0300

    Add tests

--- project/task1.py
+++ project/task1.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import Any
 import cfpq_data
 import networkx
 
@@ -8,18 +7,22 @@ import networkx
 class GraphInfo:
     nodes: int
     edges: int
-    labels: list[Any]
+    labels: list[str]
 
 
 def get_graph_info(name):
     path = cfpq_data.download(name)
     graph = cfpq_data.graph_from_csv(path)
-    return GraphInfo(graph.number_of_nodes(), graph.number_of_edges(), cfpq_data.get_sorted_labels(graph))
+    return GraphInfo(
+        graph.number_of_nodes(),
+        graph.number_of_edges(),
+        cfpq_data.get_sorted_labels(graph),
+    )
 
 
 def save_to_pydot_labeled_two_cycles_graph(n, m, labels, path):
-    graph = cfpq_data.labeled_two_cycles_graph(n, m, labels)
-    pydot_graph = networkx.drawing.nx_pydot.to_pydot(graph)
-    f = open(path, "w")
-    f.write(pydot_graph.to_string().replace("\n", ""))
-    f.close()
+    graph = cfpq_data.labeled_two_cycles_graph(n, m, labels=labels)
+    networkx.drawing.nx_pydot.write_dot(graph, path)
+
+
+save_to_pydot_labeled_two_cycles_graph(3, 2, ["a", "b"], "a.dot")
