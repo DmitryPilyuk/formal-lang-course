12e523f24b53bd7748779ca519ac4b62a50f8f8b
Dmitry Pilyuk <dmitriipiliuk@gmail.com>
2024-10-21 20:21:30 +0300

    Add hellings alg

--- project/task6.py
+++ project/task6.py
@@ -16,4 +16,51 @@ def hellings_based_cfpq(
     start_nodes: set[int] = None,
     final_nodes: set[int] = None,
 ) -> set[tuple[int, int]]:
-    pass
+    wcnf = cfg_to_weak_normal_form(cfg)
+
+    nullable = wcnf.get_nullable_symbols()
+    p_terms = {prod for prod in wcnf.productions if len(prod.body) == 1}
+    p_vars = {prod for prod in wcnf.productions if len(prod.body) == 2}
+
+    r = {(node, var, node) for node in graph.nodes for var in nullable} | {
+        (u, prod.head, v)
+        for (u, v, data) in graph.edges(data=True)
+        for prod in p_terms
+        if prod.body[0].value == data.get("label")
+    }
+
+    new = r.copy()
+
+    while new:
+        (n, N, m) = new.pop()
+        good = {
+            (u, prod.head, m)
+            for (u, M, v) in r
+            for prod in p_vars
+            if v == n
+            and prod.body[0].value == M
+            and prod.body[1].value == N
+            and (u, prod.head, m) not in r
+        }
+        new |= good
+        r |= good
+
+        good = {
+            (n, prod.head, v)
+            for (u, M, v) in r
+            for prod in p_vars
+            if m == u
+            and prod.body[0].value == N
+            and prod.body[1].value == M
+            and (n, prod.head, v) not in r
+        }
+        new |= good
+        r |= good
+
+    return {
+        (u, v)
+        for (u, var, v) in r
+        if (not start_nodes or u in start_nodes)
+        and (not final_nodes or v in final_nodes)
+        and var == wcnf.start_symbol.value
+    }
